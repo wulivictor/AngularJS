16099f06e213932984042a374b0dce908f03e7a8
freedream688 <1351698648@qq.com>
2018-04-07 14:55:53 +0800

    修复几个地方

--- bootstrap/project/js/main.js
+++ bootstrap/project/js/main.js
@@ -4,29 +4,32 @@ $(function () {
 
     //判断屏幕大小
     setting_slide_img();
+
     function setting_slide_img() {
-        var small_broswer=window.innerWidth<769;
-        var list=$('#myCarousel .item');
-        if(small_broswer){
-            list.each(function (index,ele) {
-                var ele=$(ele);
-                var img=document.createElement('img');
-                $(img).attr('src',ele.attr('data-small-img'));
-                ele.html('<img src="'+ele.attr('data-small-img')+'" alt="" />');
+        var small_broswer = window.innerWidth < 769;
+        var list = $('#myCarousel .item');
+        if (small_broswer) {
+            list.each(function (index, ele) {
+                var ele = $(ele);
+                var img = document.createElement('img');
+                $(img).attr('src', ele.attr('data-small-img'));
+                ele.html('<img src="' + ele.attr('data-small-img') + '" alt="" />');
             })
-        }else {
-            list.each(function (index,ele) {
+        } else {
+            list.each(function (index, ele) {
                 //如果是大屏幕则用小盒子包裹大盒子的方式来居中
-                var ele=$(ele);
+                var ele = $(ele);
                 ele.html('');
                 // $(ele).attr('data-big-img')
-                ele.css("background-image","url("+ele.attr('data-big-img')+")");
+                ele.css("background-image", "url(" + ele.attr('data-big-img') + ")");
             })
         }
     }
+
     //处理resize事件
-    $(window).resize(function(){
+    $(window).resize(function () {
         setting_slide_img();
+        scrollnav();
     });
 
 
@@ -38,15 +41,20 @@ $(function () {
 
 
     //给推荐栏的ul设置宽度
-    // li.offsetWidth
-    if(window.innerWidth<799){
-        var li_lists=$("#buy ul li[role='presentation']");
-        var li_lengths=0;
-        li_lists.each(function (index,ele) {
-            li_lengths=li_lengths+ele.offsetWidth;
+    scrollnav();
+    function scrollnav() {
+        var li_lists = $("#buy ul li");
+        var li_lengths = 0;
+        li_lists.each(function (index, ele) {
+            li_lengths = li_lengths + ele.offsetWidth;
         })
-        $("#buy ul").width(li_lengths);
+        if (window.innerWidth< li_lengths) {
+            $("#buy ul").width(li_lengths);
+        }
     }
 
+
+
+    
 
 })
